30342f12623db762f07a2a6625a0c3f11e1bc2ee
Davian Hurmal <davian.hurmal@gmail.com>
2023-01-23 08:41:30 -0700

    Added the code and unit test for Catalog Service as well as the deployment pipleline with Github Action

---
+++ src/main/java/com/polarbookshop/catalogservice/exception/BookAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.polarbookshop.catalogservice.exception;
+
+public class BookAlreadyExistsException extends RuntimeException{
+
+    public BookAlreadyExistsException(String isbn){
+        super("A book with ISBN " + isbn + " already exists.");
+    }
+}
