8838c1fb3697eb718aaded442163dac91a83bcfa
Davian Hurmal <davian.hurmal@gmail.com>
2023-02-07 08:31:22 -0700

    Fix the structure of the project

--- catalog-service/src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
@@ -2,12 +2,14 @@ package com.polarbookshop.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 /**
  * Provides functionality for managing the books in the catalog
  */
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
